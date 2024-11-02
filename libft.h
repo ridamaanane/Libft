28391873f392db79df924125c58f0e3c550c6fd8
Rida Maanane <rmaanane@c4r3p2.1337.ma>
2024-11-02 18:56:03 +0100

    rida

--- libft.h
+++ libft.h
@@ -35,6 +35,7 @@ char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
 char **ft_split(const char *s, char c);
 char *ft_itoa(int n);
+char    *ft_strmapi(const char *s, char (*f)(unsigned int, char));
 
 #endif
 
